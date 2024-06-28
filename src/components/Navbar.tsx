cc5f644242d2832f1e915dda548db0026d2277a4
Jesper Martinsson <jesper.martinsson92@gmail.com>
2024-06-28 20:51:20 +0200

    social linkt

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react"
 import { AiOutlinePlus } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+import { GrGithub } from "react-icons/gr";
 
 
 
@@ -64,10 +67,17 @@ return (
         MozTransition: "all .9s ease"}} className={`w-full fixed top-0 px-4 flex flex-col overflow-hidden ${navbarColor} ${nav()}`}>
         <div className="flex gap-10 justify-between lg:justify-start w-full max-w-7xl mx-auto self-center mt-2">
             <h1 className="font-mono text-3xl self-center">martinsson<span className="text-indigo-500">.</span>io</h1>
+            <div className="flex justify-between w-full">
             <div className="hidden font-mono gap-5 lg:flex text-lg self-center ">
                 <Link href={"/"} className="hover:text-indigo-500 underline underline-offset-8">Home</Link>
                 <Link href={"/"} className="hover:text-indigo-500">Blog</Link>
                 <Link href={"/"} className="hover:text-indigo-500">About</Link>
+            </div>
+            <div className="hidden font-mono lg:flex text-lg self-center h-full gap-5">
+            <FaLinkedin className="h-6 w-6 self-center hover:text-indigo-700 cursor-pointer" />
+            <GrInstagram  className="h-6 w-6 self-center hover:text-indigo-700 cursor-pointer" />
+            <GrGithub className="h-6 w-6 self-center hover:text-indigo-700 cursor-pointer" />
+            </div>
             </div>
             <div className="flex font-mono gap-5 lg:hidden text-base self-center ">
             <AiOutlinePlus style={{
@@ -80,7 +90,7 @@ return (
         <div  style={{
         transition: "all .9s ease",
         WebkitTransition: "all .9s ease",
-        MozTransition: "all .9s ease"}} className={`flex flex-col ${nav()}`}>
+        MozTransition: "all .9s ease"}} className={`flex flex-col ${nav()} justify-between h-screen`}>
             <div className="font-mono gap-5 flex flex-col text-2xl mt-10 self-center w-full text-center">
                 <div className="hover:bg-indigo-100 w-full py-3 rounded-lg">
                   <Link onClick={() => setNavRot(!navRot)} href={"/"} className="hover:text-indigo-500 underline underline-offset-8">Home</Link>  
@@ -92,6 +102,11 @@ return (
                   <Link onClick={() => setNavRot(!navRot)} href={"/about"} className="hover:text-indigo-500">About</Link>  
                 </div>
 
+            </div>
+            <div className="font-mono flex text-lg mb-10 mx-auto gap-20 ">
+            <FaLinkedin className="h-6 w-6  hover:text-indigo-700 cursor-pointer" />
+            <GrInstagram  className="h-6 w-6 hover:text-indigo-700 cursor-pointer" />
+            <GrGithub className="h-6 w-6  hover:text-indigo-700 cursor-pointer" />
             </div>
 
         </div>
