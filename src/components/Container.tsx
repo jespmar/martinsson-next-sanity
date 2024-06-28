25d08d0cae72ba5c91f1989652750bdd987f5b29
Jesper Martinsson <jesper.martinsson92@gmail.com>
2024-06-28 08:58:13 +0200

    fixed some padding

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full p-20 pt-40 max-w-7xl mx-auto">
+    <div className="w-full p-5 pt-20 lg:p-20 lg:pt-40 max-w-7xl mx-auto">
 
       <div>
         {children}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useWindowScroll } from "@uidotdev/usehooks";
 import Link from "next/link";
 import { useRouter } from "next/router";
