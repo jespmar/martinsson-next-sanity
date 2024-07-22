f352a497082840e5706fd38be5d793ba13316676
Jesper Martinsson <jesper.martinsson92@gmail.com>
2024-07-22 21:49:24 +0200

    updated links in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import { useWindowScroll } from "@uidotdev/usehooks";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react"
 import { AiOutlinePlus } from "react-icons/ai";
 import { FaLinkedin } from "react-icons/fa";
@@ -17,6 +18,13 @@ import { SharedPageProps } from "~/pages/_app";
 
 
 export const Navbar = () => {
+
+    const router = useRouter()
+
+    const isActive = (route:string) => {
+
+        if (router.asPath === `/${route}`) return "underline"
+    }
 
     
     useEffect(() => {
@@ -89,18 +97,21 @@ return (
             <Link href={"/"} className="font-mono text-3xl self-center">martinsson<span className="text-indigo-500">.</span>io</Link>
             <div className="flex justify-between w-full">
             <div className="hidden font-mono gap-5 lg:flex text-lg self-center ">
-                <Link href={"/"} className="hover:text-indigo-500 underline underline-offset-8">Blog</Link>
-                <Link href={"/archive"} className="hover:text-indigo-500">Archive</Link>
+                <Link href={"/"} className={`hover:text-indigo-500 underline-offset-8 ${isActive("")}`}>Blog</Link>
+                <Link href={"/archive"} className={`hover:text-indigo-500 underline-offset-8 ${isActive("archive")}`}>Archive</Link>
                 {navLinks && navLinks.map((link:any, index:number) => {
                     return (
-                        <Link key={index} href={`/${link.route}`} className="hover:text-indigo-500">{link.title}</Link>
+                        <Link key={index} href={`/${link.route}`} className={`hover:text-indigo-500 underline-offset-8 ${isActive(link.route)}`}>{link.title}</Link>
                     )
                 })}
             </div>
             <div className="hidden font-mono lg:flex text-lg self-center h-full gap-5">
+            <a target="blank" title="linkedin" className="self-center" href="https://www.linkedin.com/in/jesper-martinsson-372007131">
             <FaLinkedin className="h-6 w-6 self-center hover:text-indigo-700 cursor-pointer" />
-            <GrInstagram  className="h-6 w-6 self-center hover:text-indigo-700 cursor-pointer" />
+            </a>
+            <a target="blank" title="github" className="self-center" href="https://github.com/jespmar">
             <GrGithub className="h-6 w-6 self-center hover:text-indigo-700 cursor-pointer" />
+            </a>
             </div>
             </div>
             <div className="flex font-mono gap-5 lg:hidden text-base self-center ">
@@ -116,24 +127,26 @@ return (
         WebkitTransition: "all .9s ease",
         MozTransition: "all .9s ease"}} className={`flex flex-col ${nav()} h-screen gap-20`}>
             <div className="font-mono gap-5 flex flex-col text-2xl mt-10 self-center w-full text-center">
-                  <Link onClick={() => setNavRot(!navRot)} href={"/"} className="hover:text-indigo-500 underline underline-offset-8 hover:bg-indigo-100 w-full py-3 rounded-lg">Blog</Link>  
+                  <Link onClick={() => setNavRot(!navRot)} href={"/"} className={`hover:text-indigo-500 hover:bg-indigo-100 w-full py-3 rounded-lg ${isActive("")}`}>Blog</Link>  
 
-                  <Link onClick={() => setNavRot(!navRot)} href={"/archive"} className="hover:text-indigo-500 hover:bg-indigo-100 w-full py-3 rounded-lg">Archive</Link>  
+                  <Link onClick={() => setNavRot(!navRot)} href={"/archive"} className={`hover:text-indigo-500 hover:bg-indigo-100 w-full py-3 rounded-lg ${isActive("archive")}`}>Archive</Link>  
 
 
                 {navLinks && navLinks.map((link:any, index:number) => {
                     return (
                         
-                  <Link key={index} onClick={() => setNavRot(!navRot)} href={`/${link.route}`} className="hover:bg-indigo-100 w-full py-3 rounded-lg">{link.title}</Link>  
+                  <Link key={index} onClick={() => setNavRot(!navRot)} href={`/${link.route}`} className={`hover:text-indigo-500 hover:bg-indigo-100 w-full py-3 rounded-lg ${isActive(link.route)}`}>{link.title}</Link>  
                 
                     )
                 })}
 
             </div>
             <div className="font-mono flex text-lg mb-10 mx-auto gap-20 ">
-            <FaLinkedin className="h-6 w-6  hover:text-indigo-700 cursor-pointer" />
-            <GrInstagram  className="h-6 w-6 hover:text-indigo-700 cursor-pointer" />
-            <GrGithub className="h-6 w-6  hover:text-indigo-700 cursor-pointer" />
+                <a target="blank" title="linkedin" className="cursor-pointer" href="https://www.linkedin.com/in/jesper-martinsson-372007131">
+                <FaLinkedin  className="h-6 w-6  hover:text-indigo-700 cursor-pointer" />
+                </a>
+                <a target="blank" title="github" className="self-center" href="https://github.com/jespmar">
+                <GrGithub className="h-6 w-6  hover:text-indigo-700 cursor-pointer" />            </a>
             </div>
 
         </div>
